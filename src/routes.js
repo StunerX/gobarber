a9bc1ae98ad4b9ec19f0a09bfbfd6eedb971bc1b
Cleyton Santos <cleyton.a.santos@gmail.com>
2020-01-28 19:50:23 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json('Go Barber'));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Cleyton Santos',
+        email: 'cleyton.a.santos@gmail.com',
+        password_hash: '123123123'
+    });
+
+    res.json(user);
+});
 
 export default routes;
