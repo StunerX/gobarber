af0298e33e5621d7dce3778a881202e781c01b66
Cleyton Santos <cleyton.a.santos@gmail.com>
2020-01-28 20:09:56 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Cleyton Santos',
-        email: 'cleyton.a.santos@gmail.com',
-        password_hash: '123123123'
-    });
-
-    res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
